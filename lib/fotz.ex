87bb2e8426b0e5b8ce5243d106adfd52a7daf140
Eber Freitas Dias <eber.freitas@gmail.com>
2019-07-22 08:53:47 -0300

    Starting processing

--- lib/fotz.ex
+++ lib/fotz.ex
@@ -9,18 +9,57 @@ defmodule Fotz do
   ]
 
   @maps_endpoint "https://api.opencagedata.com/geocode/v1/json"
-  @api_key "39cf00e67a544a30ae768042a4b51083"
-  @language "pt-BR"
+
+  def main(argv) do
+    argv
+    |> parse_opts()
+    |> process()
+  end
+
+  def parse_opts(argv) do
+    argv
+    |> OptionParser.parse(
+      strict: [
+        help: :boolean
+        source: :string,
+        dest: :string,
+        geokey: :string,
+        language: :string,
+        move: :boolean,
+        config: :string
+      ],
+      aliases: [
+        h: :help,
+        s: :source,
+        d: :dest,
+        k: :geokey,
+        l: :language,
+        m: :move,
+        c: :config
+      ]
+    )
+    |> elem(0)
+    |> Enum.into(%{})
+  end
+
+  def process(%{source: _, dest: _} = opts) do
+    opts
+  end
+
+  def process(_) do
+    exit(:normal)
+  end
 
   def files_from_dir(dir) do
     dir =
       dir
       |> String.trim()
+      |> String.replace("\\", "/")
       |> Path.expand()
       |> String.trim_trailing("/")
 
     joined_extensions = Enum.join(@extensions, ",")
-    joined_up_extensions = Enum.join(Enum.map(@extensions, &String.upcase/1))
+    joined_up_extensions = String.upcase(joined_extensions)
 
     (dir <> "/**/*.{#{joined_extensions},#{joined_up_extensions}}")
     |> Path.wildcard()
@@ -55,15 +94,17 @@ defmodule Fotz do
 
     {:ok, direction} = Keyword.fetch(directions, direction)
 
-    (degrees + minutes / 60 + seconds / 3600) * direction
+    ((degrees + minutes / 60 + seconds / 3600) * direction)
     |> Float.round(6)
   end
 
-  def gps(lat, lng) do
+  def gps(lat, lng, api_key, language \\ nil) do
+    language = language || "native"
+
     query = %{
       q: "#{lat},#{lng}",
-      key: @api_key,
-      language: @language
+      key: api_key,
+      language: language
     }
 
     url = @maps_endpoint <> "?" <> URI.encode_query(query)
