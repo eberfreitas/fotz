b7731eeff7d0298fbd8acf58c612558c282108b9
Eber Freitas Dias <eber.freitas@gmail.com>
2019-08-01 13:33:00 -0300

    Finishing files test

--- test/fotz/files_test.exs
+++ test/fotz/files_test.exs
@@ -10,4 +10,13 @@ defmodule Fotz.FilesTest do
 
     assert length(files) == 2
   end
+
+  test "file extension" do
+    assert Files.file_extension("test.jpg") == "jpg"
+    assert Files.file_extension("./dir/test.PNG") == "png"
+  end
+
+  test "md5 file" do
+    assert Files.md5("./test/_samples/001.jpg") == "5f69d520fb4308355453b4060eb456fa"
+  end
 end
