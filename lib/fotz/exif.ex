ae4fa95b8df760612bb1fca9e054fe0f9539d7fb
Eber Freitas Dias <eber.freitas@gmail.com>
2019-08-04 04:08:37 -0300

    Exiftool check

--- lib/fotz/exif.ex
+++ lib/fotz/exif.ex
@@ -11,16 +11,34 @@ defmodule Fotz.Exif do
     "FileModifyDate"
   ]
 
+  @exiftool "exiftool"
+
+  @doc """
+  Checks if exiftool is properly installed and can be called from the app.
+  """
+  @spec exiftool?() :: boolean
+  def exiftool?() do
+    try do
+      {_, 0} = System.cmd(@exiftool, [])
+
+      true
+    rescue
+      ErlangError -> false
+    end
+  end
+
   @doc """
   Receives a `file` param from which we extract exif data using `exiftool`. The
   `file` param must be a string of the full path of the file.
   """
-  @spec exif(String.t()) :: map
+  @spec exif(String.t()) :: :error | map
   def exif(file) do
-    {json, 0} = System.cmd("exiftool", ["-json", "-q", file])
-    {:ok, [data | _]} = Jason.decode(json)
-
-    data
+    with {json, 0} <- System.cmd(@exiftool, ["-json", "-q", file]),
+         {:ok, [data | _]} <- Jason.decode(json) do
+      data
+    else
+      _ -> :error
+    end
   end
 
   @doc """
@@ -29,14 +47,23 @@ defmodule Fotz.Exif do
   inspected. Gets the oldest date from the available keys and returns a
   NaiveDateTime value.
   """
-  @spec get_date(map) :: NaiveDateTime.t()
+  @spec get_date(map) :: :error | NaiveDateTime.t()
   def get_date(exif) do
-    exif
-    |> Enum.filter(fn i -> elem(i, 0) in @dates end)
-    |> Enum.map(fn i -> make_valid_date(elem(i, 1)) end)
-    |> Enum.reduce(NaiveDateTime.utc_now(), fn i, acc ->
-      if i < acc, do: i, else: acc
-    end)
+    dates =
+      exif
+      |> Enum.filter(fn i -> elem(i, 0) in @dates end)
+
+    cond do
+      Enum.count(dates) > 0 ->
+        dates
+        |> Enum.map(fn i -> make_valid_date(elem(i, 1)) end)
+        |> Enum.reduce(NaiveDateTime.utc_now(), fn i, acc ->
+          if i < acc, do: i, else: acc
+        end)
+
+      true ->
+        :error
+    end
   end
 
   @spec make_valid_date(String.t()) :: NaiveDateTime.t()
